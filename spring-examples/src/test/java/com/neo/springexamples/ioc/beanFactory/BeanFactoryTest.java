a2c3ecbf7f4052506ad87944bc8c3bd1a6d74f46
wangkun <wangkun@fenqi.im>
2018-08-19 23:11:50 +0800

    test1

--- spring-examples/src/test/java/com/neo/springexamples/ioc/beanFactory/BeanFactoryTest.java
+++ spring-examples/src/test/java/com/neo/springexamples/ioc/beanFactory/BeanFactoryTest.java
@@ -1,21 +1,24 @@
 package com.neo.springexamples.ioc.beanFactory;
 
-import com.google.gson.Gson;
-import com.neo.springexamples.ioc.beanFactory.pojo.DemoBean;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import com.google.gson.Gson;
+import com.neo.springexamples.ioc.beanFactory.pojo.DemoBean;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class BeanFactoryTest {
 
-    private final Gson gson = new Gson();
+    private final Gson   gson = new Gson();
 
-    private final String pre = "------------------------- ";
+    private final String pre  = "------------------------- ";
 
     @Test
     public void test1() {
@@ -23,10 +26,26 @@ public class BeanFactoryTest {
         XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(factory);
         Resource resource = new ClassPathResource("beanFactory.xml");
         xmlBeanDefinitionReader.loadBeanDefinitions(resource);
+
+        BeanDefinition demoBean = factory.getBeanDefinition("demoBean");
+        Assert.assertNotNull(demoBean);
+
+        //        无法通过alias获取beanDefinition
+        //        BeanDefinition demoBean2 = factory.getBeanDefinition("demoBean2");
+        //        Assert.assertNotNull(demoBean2);
+
         DemoBean bean = factory.getBean(DemoBean.class);
 
         System.out.println(gson.toJson(bean));
         Assert.assertNotNull(bean);
+
+        String[] beanDefinitionNames = factory.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            System.out.println(beanDefinitionName);
+            ;
+
+        }
+
     }
 
     /**
@@ -43,17 +62,16 @@ public class BeanFactoryTest {
         String[] demoBeans = factory.getAliases(id);
 
         if (demoBeans.length == 0) { // 如果所有name和id一样，那么就没有alias
-            log.info("{} {} has no alias",pre, id);
+            log.info("{} {} has no alias", pre, id);
         }
 
         DemoBean bean = factory.getBean(DemoBean.class);
         for (String demoBeanAlias : demoBeans) {
-            log.info("{} has alias {}",pre, demoBeanAlias);
+            log.info("{} has alias {}", pre, demoBeanAlias);
             DemoBean bean2 = factory.getBean(demoBeanAlias, DemoBean.class);
-            Assert.assertEquals(bean,bean2);
+            Assert.assertEquals(bean, bean2);
         }
 
     }
-
 
 }
